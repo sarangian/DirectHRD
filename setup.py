7489fc60f75af212a47bdc7c185b245728e7a472
Aditya Narayan Sarangi <a.n.sarangi@gmail.com>
2024-07-19 13:38:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         # List your dependencies here
-        'pandas=2.0.3',
+        'pandas==2.0.3',
         'numpy',
         'pandas',
         'scikit-learn',
