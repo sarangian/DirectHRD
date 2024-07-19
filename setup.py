29509ab957f9a51b9c1601bc3ed339081ad042b8
Aditya Narayan Sarangi <a.n.sarangi@gmail.com>
2024-07-19 11:52:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
     packages=find_packages(),
     install_requires=[
         # List your dependencies here
-        'pandas>=2.1.2',
-        'numpy',
+        'numpy<2.0.0',
+        'pandas',
         'scikit-learn',
         'sigproSS',
         'sigProfilerPlotting',
